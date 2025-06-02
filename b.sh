2bea0d192249a94f33cf0cdc478d6c195997346c
Onkar Kumbhar <onkarkumbhar1628@gmail.com>
2025-06-02 22:24:33 +0530

    second

---
+++ b.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prompt the user for two numbers
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+# Perform subtraction using arithmetic expansion
+difference=$((num1 - num2))
+
+# Display the result
+echo "The difference is: $difference"
+echo "Hii"
+echo "hello"
